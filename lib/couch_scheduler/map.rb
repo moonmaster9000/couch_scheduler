9446195b60101cd0f5276c133cd60be657d7671c
Matt Parker <moonmaster9000@gmail.com>
2011-08-17 14:33:20 -0400

    default the start to now, instead of an arbitrary date in the past

--- lib/couch_scheduler/map.rb
+++ lib/couch_scheduler/map.rb
@@ -10,7 +10,7 @@ module CouchScheduler
               var start = new Date(doc.start)
               start.setDate(start.getDate() + 1)
             } else {
-              var start = new Date(2011, 0, 1, 0, 0, 0, 0)
+              var start = new Date()
             }
 
             if (doc.end){
